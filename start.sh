0b2e1e6b21fad72b17dd5b15da2eac4305713a99
jaelinny <leenlyn3127@gmail.com>
2024-02-27 16:47:15 +0900

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 figlet start
-nohup java -jar ~/app/JOB_NAME/rest-service-0.0.1-SNAPSHOT.jar >> ~/app/JOM_NAME/log.log &
-echo $! > ~/app/JOM_NAME/pid.file
+nohup java -jar ~/app/JOB_NAME/rest-service-0.0.1-SNAPSHOT.jar >> ~/app/JOB_NAME/log.log &
+echo $! > ~/app/JOB_NAME/pid.file
